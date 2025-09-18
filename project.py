1f71d5c5a63ef97550742cbfc9e40ac0d3e42f55
Superior-Snacks <joningiwork@gmail.com>
2025-09-18 16:57:17 +0000

    s

--- project.py
+++ project.py
@@ -8,6 +8,14 @@ length = {
         # Imperial → meters
         "in": 0.0254,"ft": 0.3048,"yd": 0.9144,"ch": 20.1168,"fur": 201.168,"mi": 1609.344,"lea": 4828.032
     }
+weight = {
+        # Metric (SI, in grams)
+        "yg": 1e-24,"zg": 1e-21,"ag": 1e-18,"fg": 1e-15,"pg": 1e-12,"ng": 1e-9,"µg": 1e-6,"mg": 1e-3,"cg": 1e-2,"dg": 1e-1,
+        "g": 1.0,"dag": 1e1,"hg": 1e2,"kg": 1e3,"Mg": 1e6,
+        # Imperial → grams
+        "gr": 0.06479891,"dr": 1.771845,"oz": 28.349523125,"lb": 453.59237,"st": 6350.29318,
+        "qr": 12700.58636,"cwt_us": 45359.237,"cwt_uk": 50802.34544,"ton_us": 907184.74,"ton_uk": 1016046.9088
+    }
 def main():
     display()
 
