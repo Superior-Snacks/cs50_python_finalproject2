c9f17adc5e4308f3c4d9a5e02e78a9c1618538ac
Superior-Snacks <joningiwork@gmail.com>
2025-09-18 20:16:06 +0000

    s

--- project.py
+++ project.py
@@ -28,6 +28,7 @@ volume = {
     "floz_uk": 0.0284131,"gill_uk": 0.142065,"pt_uk": 0.568261, "qt_uk": 1.13652,"gal_uk": 4.54609}
 def main():
     app = Display
+    app.root.mainloop()
 
 def convert_weight(amount, old, new):
     if old.lower() == new.lower():
