22c2526e4b527074b31ba40b59568bcbf606aa84
Superior-Snacks <joningiwork@gmail.com>
2025-09-18 16:55:24 +0000

    s

--- project.py
+++ project.py
@@ -1,16 +1,18 @@
 import tkinter
 from tkinter import ttk
 
-units = {
-    "metric_lenght":[eniter scope of metric lenght units],
-    "imperial_lenght":[eniter scope of imperial lenght units],
-    "metric_weight":[eniter scope of metric weight units],
-    "imperial_weight":[eniter scope of imperial weight units],
-}
+length = {
+        # Metric (SI, in meters)
+        "ym": 1e-24,"zm": 1e-21,"am": 1e-18,"fm": 1e-15,"pm": 1e-12,"nm": 1e-9,"µm": 1e-6,"mm": 1e-3,"cm": 1e-2,"dm": 1e-1,
+        "m": 1.0,"dam": 1e1,"hm": 1e2,"km": 1e3,"Mm": 1e6,"Gm": 1e9,"Tm": 1e12,
+        # Imperial → meters
+        "in": 0.0254,"ft": 0.3048,"yd": 0.9144,"ch": 20.1168,"fur": 201.168,"mi": 1609.344,"lea": 4828.032
+    }
 def main():
     display()
 
-def convert_weight(x, untitx, y, unity):
+def convert_weight(x, untitx, unity):
+    ...
     
 
 
