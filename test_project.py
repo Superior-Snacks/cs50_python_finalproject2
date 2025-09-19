a51741744e4d386b2c1e448e1994c09116265417
Superior-Snacks <joningiwork@gmail.com>
2025-09-19 14:51:26 +0000

    s

--- test_project.py
+++ test_project.py
@@ -7,19 +7,37 @@ def test_convert_weight():
     assert convert_weight(1, "ton_us", "kg") == 907.18474
 
 def test_convert_volume():
-    ...
+    assert convert_volume() ==
+    assert convert_volume() ==
+    assert convert_volume() ==
+    assert convert_volume() ==
 
 def test_convert_currency():
-    ...
+    assert convert_currency() ==
+    assert convert_currency() ==
+    assert convert_currency() ==
+    assert convert_currency() ==
 
 def test_convert_lenght():
-    ...
+    assert convert_length() ==
+    assert convert_length() ==
+    assert convert_length() ==
+    assert convert_length() ==
 
 def test_convert_temperature():
-    ...
+    assert convert_temperature() ==
+    assert convert_temperature() ==
+    assert convert_temperature() ==
+    assert convert_temperature() ==
 
 def test_get_currency():
-    ...
+    assert get_currency() ==
+    assert get_currency() ==
+    assert get_currency() ==
+    assert get_currency() ==
 
 def test_decide():
-    ...
+    assert decide() ==
+    assert decide() ==
+    assert decide() ==
+    assert decide() ==
