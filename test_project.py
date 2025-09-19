aa8b29debcb79b267dc34a96d1b5bab45d603525
Superior-Snacks <joningiwork@gmail.com>
2025-09-19 14:47:42 +0000

    s

--- test_project.py
+++ test_project.py
@@ -2,9 +2,9 @@ from project import convert_weight, convert_volume, convert_currency, convert_le
 
 def test_convert_weight():
     assert convert_weight(1, "kg", "g") == 1000
-    assert convert_weight(1, "kg", "g") == 1000
-    assert convert_weight(1, "kg", "g") == 1000
-    assert convert_weight(1, "kg", "g") == 1000
+    assert convert_weight(1, "mg", "yg") == 1.0000000000000001e+21
+    assert convert_weight(1, "ton_uk", "kg") == 1016.0469088
+    assert convert_weight(1, "ton_us", "kg") == 907.18474
 
 def test_convert_volume():
     ...
