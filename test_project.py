1bf0dca58e72be5913fac826cb9c713aaeb903f3
Superior-Snacks <joningiwork@gmail.com>
2025-09-19 15:03:29 +0000

    s

--- test_project.py
+++ test_project.py
@@ -19,10 +19,10 @@ def test_convert_lenght():
     assert convert_length(1, "mi", "m") == 1609.344
 
 def test_convert_temperature():
-    assert convert_temperature() ==
-    assert convert_temperature() ==
-    assert convert_temperature() ==
-    assert convert_temperature() ==
+    assert convert_temperature(1, "C", "F") == 33.8
+    assert convert_temperature(1, "f", "K") == 255.92777777777775
+    assert convert_temperature(1, "k", "C") == -272.15
+    assert convert_temperature(1, "f", "c") == -17.22222222222222
 
 def test_convert_currency():
     assert convert_currency() ==
